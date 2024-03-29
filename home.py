2495debfa15faf68298d2f00a549fa8cd514b1d5
Avanish Yadav <91160609+awanishyadav967@users.noreply.github.com>
2024-03-29 08:41:52 +0530

    Update home.py

--- home.py
+++ home.py
@@ -4,8 +4,8 @@ from modeci_mdf.utils import simple_connect
 from modeci_mdf.execution_engine import EvaluableGraph
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image as PILImage
-#from IPython.display import Image
+#from PIL import Image as PILImage
+from IPython.display import Image
 
 def app():
     st.title("Multi-Mass System Simulation")
@@ -82,8 +82,8 @@ def execute_model():
 
     # Display the generated graph image
     st.write("Displaying the generated graph image")
-    #st.image(graph_image_file)
-    pil_image = PILImage.open(graph_image_file)
+    st.image(graph_image_file)
+    #pil_image = PILImage.open(graph_image_file)
 
 
      # Define time array
